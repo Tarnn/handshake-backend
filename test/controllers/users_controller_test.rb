20b2c9e9d76320e2eee32e43fa5d50e7709c1add
Tarnn <taranjitk18@gmail.com>
2025-03-21 19:18:13 -0400

    feat: added stubs + cors SHAKE-03

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -10,43 +10,38 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'should get index' do
-    get '/index', headers: @headers
-    assert_response :success, "Expected successful response"
+    expected_users = [
+      { 'id' => @jim.id, 'first_name' => 'Jim', 'last_name' => 'Hawkins', 'check_in_time' => @jim.check_in_time.as_json },
+      { 'id' => @sally.id, 'first_name' => 'Sally', 'last_name' => 'Ride', 'check_in_time' => @sally.check_in_time.as_json }
+    ]
+    
+    UserService.stub :fetch_all_users, [@jim, @sally] do
+      get '/index', headers: @headers
+      assert_response :success, "Expected successful response"
 
-    users = JSON.parse(response.body)
-    assert_kind_of Array, users, "Response should be an array"
-    assert_equal 2, users.length, "Should have exactly 2 users"
-    
-    # Test first user
-    assert_equal 'Jim', users[0]['first_name'], "First user's first name should be Jim"
-    assert_equal 'Hawkins', users[0]['last_name'], "First user's last name should be Hawkins"
-    
-    # Test second user
-    assert_equal 'Sally', users[1]['first_name'], "Second user's first name should be Sally"
-    assert_equal 'Ride', users[1]['last_name'], "Second user's last name should be Ride"
+      users = JSON.parse(response.body)
+      assert_equal expected_users, users, "Response should match expected users"
+    end
   end
 
   test 'should handle errors gracefully' do
-    # Override the UserService method to raise an error
-    def UserService.fetch_all_users
-      raise StandardError, "Test error"
+    # Use stub to temporarily override the method for this test only
+    UserService.stub :fetch_all_users, ->{ raise StandardError, "Test error" } do
+      get '/index', headers: @headers
+      assert_response :internal_server_error, "Should return 500 status code on error"
+      
+      json_response = JSON.parse(response.body)
+      assert_includes json_response["errors"]["base"], "Error fetching users", "Error message should be present"
     end
-    
-    get '/index', headers: @headers
-    assert_response :internal_server_error, "Should return 500 status code on error"
-    
-    json_response = JSON.parse(response.body)
-    assert_includes json_response["errors"]["base"], "Error fetching users", "Error message should be present"
   end
 
-  # Uncomment these tests when authentication is enabled
-  # test "should require authentication" do
-  #   get users_url
-  #   assert_response :unauthorized
-  # end
-  #
-  # test "should allow access with valid token" do
-  #   get users_url, headers: { 'Authorization' => 'Bearer your-secret-token' }
-  #   assert_response :success
-  # end
+  test "should require authentication" do
+    get '/index', headers: {}
+    assert_response :unauthorized
+  end
+  
+  test "should allow access with valid token" do
+    get '/index', headers: { 'Authorization' => 'Bearer your-secret-token' }
+    assert_response :success
+  end
 end 
